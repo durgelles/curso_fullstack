0df759f27870b12a0c353457be81f6a1bf6a60e8
Dennis Urgelles <durgellescabrera@gmail.com>
2026-07-05 21:07:37 -0600

    pillando soltura

--- listas_numericas.py
+++ listas_numericas.py
@@ -13,3 +13,48 @@ compresión).
 8. Encuentra el índice correspondiente al número 8 en la lista original y en la lista resultante tras 
 el punto 2. 
 """
+#1
+numeros=list(range(1,11))
+print(numeros)
+#2
+list_pares_inverso=[]
+ 
+for num in numeros:
+    if num%2==0:
+     list_pares_inverso.append(num)
+
+for num in list_pares_inverso[::-1]:
+     print(num)  
+
+#3
+for num in numeros:
+   cuadrado_num=num**2
+   print(f"el cuadrado de {num} es : {cuadrado_num}") 
+
+#4
+   print(min(numeros))  
+   print(max(numeros))  
+
+#5
+suma=0
+#for i in range(11):
+   #suma+=i
+
+#print(suma)   
+
+#for num in numeros:
+   #suma+=num
+
+#print(suma)   
+#
+suma=sum(numeros)
+print(suma)
+
+#8
+for i in range(len(numeros)):
+   if numeros[i]==8:
+    print(i)
+#
+for i in range(len(list_pares_inverso)):
+   if list_pares_inverso[i]==8:
+    print(i)
