2731e027e337e26f5e81b5fec9c07e38729617f1
Dennis Urgelles <durgellescabrera@gmail.com>
2026-07-10 20:56:20 -0600

    ejer5

--- pillando_soltura/pillando_soltura5.1.py
+++ pillando_soltura/pillando_soltura5.1.py
@@ -3,11 +3,12 @@ programa, realice la suma de aquellos números que sean divisibles por este. """
 #pedir un numero al usaario:
 numero_usuario=int(input("ingrese un numero: "))
 # realizar lista hasta el 100 para ver los numeros divisibles
-numeros=list(range(1,101))
+numeros=list(range(1,numero_usuario+1))
 lista_divisible=[]
 #recorrer la lista de los numeros 
+print(f"los numeros divisibles por {numero_usuario} son :")
 for num in numeros:
-    if num%numero_usuario==0:
+    if numero_usuario%num==0:
         lista_divisible.append(num)
         print(num)  
 
