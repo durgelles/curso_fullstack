2798aa9767ced6ecd0e938523881d27544a73265
Dennis Urgelles <durgellescabrera@gmail.com>
2026-09-09 18:39:30 -0600

    base

--- hoja_ejercicios3B/hoja_ejercicios3C/base_datos_colegio.py
+++ hoja_ejercicios3B/hoja_ejercicios3C/base_datos_colegio.py
@@ -10,10 +10,22 @@ media de cada estudiante. También puedes usar otro bucle para calcular la nota 
 clase. """
 
 #crear lista de nombre de estudiantes con las notas
-estudiantes=[["pedro",8,7,9]],["ruben",9,10,9],["dennis",6,7,8],["ana",9,8,10],["karla",10,9,7]
+estudiantes= [  ["pedro",8,7,9],["ruben",9,10,9],["dennis",6,7,8],["ana",9,8,10],["karla",10,9,7]]
 #crear lista de materias
 materias=["deberes","examenes","proyectos"]
+lista_notas=[]
 #recorres listas
 for i in range(len(estudiantes)):
-    for j in range(len(materias)):
-        print(f"estudiante {estudiantes[i][0]} , en la materia {materias[2]} su nota fue de {estudiantes[i][j+2]}")
+    suma=0
+    for j in range(1,len(estudiantes[i])):
+        suma+=estudiantes[i][j]
+        lista_notas.append(estudiantes[i][j])
+        
+
+    print(f" la nota media del estudiante  {estudiantes[i][0]} es {suma/3}")
+
+
+print(f" el promedio de la clase es : {sum(lista_notas)/len(lista_notas)}")
+
+
+     
